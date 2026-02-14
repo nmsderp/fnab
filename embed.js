a2d943b890f888665109dd1b7d58c4e4b0cecd1f
nmsderp <130254323+nmsderp@users.noreply.github.com>
2026-02-14 15:56:49 -0500

    h,md

---
+++ embed.js
@@ -0,0 +1,14 @@
+(function () {
+    const script = document.currentScript;
+
+    const basePath = script.src.substring(0, script.src.lastIndexOf("/") + 1);
+
+    const iframe = document.createElement("iframe");
+    iframe.src = basePath + "FNAB.html";
+    iframe.style.width = script.getAttribute("data-width") || "800px";
+    iframe.style.height = script.getAttribute("data-height") || "600px";
+    iframe.style.border = "none";
+    iframe.style.display = "block";
+
+    script.parentNode.replaceChild(iframe, script);
+})();
