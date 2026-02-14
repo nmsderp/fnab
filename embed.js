6244d4695b729fd41b97ec3471075e227675389d
nmsderp <130254323+nmsderp@users.noreply.github.com>
2026-02-14 15:59:08 -0500

    what

--- embed.js
+++ embed.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function () {
     const script = document.currentScript;
 
@@ -13,19 +12,3 @@
 
     script.parentNode.replaceChild(iframe, script);
 })();
-=======
-(function () {
-    const script = document.currentScript;
-
-    const basePath = script.src.substring(0, script.src.lastIndexOf("/") + 1);
-
-    const iframe = document.createElement("iframe");
-    iframe.src = basePath + "index.html";
-    iframe.style.width = script.getAttribute("data-width") || "800px";
-    iframe.style.height = script.getAttribute("data-height") || "600px";
-    iframe.style.border = "none";
-    iframe.style.display = "block";
-
-    script.parentNode.replaceChild(iframe, script);
-})();
->>>>>>> d7561c154973cb96da3b8ab5a88945610ffa2531
